cfcc09ca39fbe2c8df189e9a82fdaf1da249a6c3
wakamezake <hotwater1367@gmail.com>
2019-10-12 17:13:06 +0900

    add new test

---
+++ ch01/tasks/test_three.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+
+Task = namedtuple("Task", ["summary", "owner", "done", "id"])
+Task.__new__.__defaults__ = (None, None, None, None)
+
+
+def test_defaults():
+    task_1 = Task()
+    task_2 = Task(None, None, None, None)
+    assert task_1 == task_2
+
+
+def test_member_access():
+    task = Task("buy milk", "brian")
+    assert task.summary == "buy milk"
+    assert task.owner == "brian"
+    assert (task.done, task.id) == (False, None)
